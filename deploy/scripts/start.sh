80006ae5625f471407c3cf8fdc607bfa178439f8
csieflyman <csieflyman@gmail.com>
2021-07-22 08:44:36 +0800

    improve deploy scripts

--- deploy/scripts/start.sh
+++ deploy/scripts/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-source version.sh
+source configs.sh
 
 # run kill -3 $pid to print jvm thread dump and memory usage to jvm.log
-export JAVA_OPTS="-Xms128m -Xmx512m -XX:+UnlockDiagnosticVMOptions -XX:+LogVMOutput -XX:LogFile=$HOME/jvm.log"
-export APP_OPTS="-Dconfig.file="$appPath/application-$env.conf" -Dlogback.configurationFile="$appPath/logback.xml" -Dswaggerui.dir="$HOME/swagger-ui" -Dkotlinx.coroutines.debug"
+export JAVA_OPTS="-Xms128m -Xmx512m -XX:+UnlockDiagnosticVMOptions -XX:+LogVMOutput -XX:LogFile=$KTOR_LOG_PATH/jvm.log"
+export APP_OPTS="-Dconfig.file=$KTOR_APP_PATH/application-$KTOR_ENV.conf -Dlogback.configurationFile=$KTOR_APP_PATH/logback.xml -Dswaggerui.dir=$KTOR_SWAGGER_PATH -Dkotlinx.coroutines.debug"
 
-echo $JAVA_OPTS APP_OPTS
+echo "$JAVA_OPTS" "$APP_OPTS"
 echo "start..."
-$appPath/bin/ktor-example &
+"$KTOR_APP_PATH"/bin/ktor-example &
 
--- deploy/scripts/stop.sh
+++ deploy/scripts/stop.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source version.sh
-
 echo "stop..."
-curl "http://localhost:$APP_SERVER_PORT/server/shutdown/$APP_SERVER_SHUTDOWN_KEY"
+curl "http://localhost:$SERVER_PORT/server/shutdown/$SERVER_SHUTDOWN_KEY"
 
