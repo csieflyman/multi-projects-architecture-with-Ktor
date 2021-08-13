351878bdd2c04be9cb67f73549aa9577afc60f6e
csieflyman <csieflyman@gmail.com>
2021-08-13 16:24:53 +0800

    fix gradle buildSrc java-conventions.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.5.21"
-    id("org.gradle.kotlin.kotlin-dsl") version "2.1.6"
-    id("org.gradle.kotlin.embedded-kotlin") version "2.1.6"
-    //`kotlin-dsl`
+    `kotlin-dsl`
 }
 
 repositories {
