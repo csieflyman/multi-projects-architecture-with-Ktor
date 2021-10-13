9f3e93649cbb74e62b5e57bc840cbeb9ad852d8e
csieflyman <csieflyman@gmail.com>
2021-10-14 01:32:25 +0800

    add try/catch block for execute function in NotificationCoroutineActor

--- infra/src/fanpoll/infra/notification/senders/NotificationCoroutineActor.kt
+++ infra/src/fanpoll/infra/notification/senders/NotificationCoroutineActor.kt
@@ -6,17 +6,22 @@ package fanpoll.infra.notification.senders
 
 import fanpoll.infra.base.async.CoroutineActor
 import fanpoll.infra.base.async.CoroutineActorConfig
+import fanpoll.infra.base.exception.InternalServerException
 import fanpoll.infra.base.response.InfraResponseCode
+import fanpoll.infra.logging.error.ErrorLog
 import fanpoll.infra.logging.writers.LogWriter
 import fanpoll.infra.notification.Notification
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import mu.KotlinLogging
 
 class NotificationCoroutineActor(
     coroutineActorConfig: CoroutineActorConfig,
     private val notificationSender: NotificationSender,
     private val logWriter: LogWriter
 ) : NotificationSender {
+
+    private val logger = KotlinLogging.logger {}
 
     private val actorName = "NotificationActor"
 
@@ -32,7 +37,21 @@ class NotificationCoroutineActor(
     }
 
     private fun execute(notification: Notification) {
-        notificationSender.send(notification)
+        try {
+            notificationSender.send(notification)
+        } catch (e: Throwable) {
+            val errorMsg = "$actorName execute error"
+            logger.error("errorMsg => $notification", e)
+            logWriter.write(
+                ErrorLog.internal(
+                    InternalServerException(
+                        InfraResponseCode.NOTIFICATION_ERROR, errorMsg, e,
+                        mapOf("id" to notification.id, "type" to notification.type, "eventId" to notification.eventId)
+                    ),
+                    actorName, notification.id.toString()
+                )
+            )
+        }
     }
 
     override fun shutdown() {
