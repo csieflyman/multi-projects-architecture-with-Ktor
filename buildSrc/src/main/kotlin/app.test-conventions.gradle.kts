3b3098e69242f3736d34f648fe7d2cebb774d19e
csieflyman <csieflyman@gmail.com>
2021-12-14 12:36:18 +0800

    improve gradle testing

--- buildSrc/src/main/kotlin/app.test-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/app.test-conventions.gradle.kts
@@ -28,32 +28,51 @@ dependencies {
 
 tasks.withType<Test> {
 
-    useJUnitPlatform {
-        filter {
-            //setIncludePatterns("")
-            //includeTags("")
-            //excludeTags("")
-        }
-        reports {
-            junitXml.required.set(false) // CI Server => true
-            html.required.set(false) // CI Server => true
-        }
+    val env = "local-test"
+    val properties = loadProperties("$env.properties")
+
+    // https://docs.gradle.org/current/userguide/performance.html#parallel_test_execution
+    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+    minHeapSize = "256m"
+    maxHeapSize = "512m"
+
+    failFast = true
+    ignoreFailures = false
+
+    var reportJunitXmlEnabled = true
+    var reportJunitHtmlEnabled = true
+
+    if (properties != null) {
+        val gradleProps = properties.filter { it.key.startsWith("gradle.") }
+            .mapKeys { it.key.substring("gradle.".length) }.toMutableMap()
+        //println("========== Testing Gradle Properties ==========")
+        //println(gradleProps)
+
+        gradleProps["minHeapSize"]?.takeIf { it.isNotEmpty() }?.also { minHeapSize = it }
+        gradleProps["maxHeapSize"]?.takeIf { it.isNotEmpty() }?.also { maxHeapSize = it }
+        gradleProps["failFast"]?.takeIf { it.isNotEmpty() }?.also { failFast = it.toBoolean() }
+        gradleProps["ignoreFailures"]?.takeIf { it.isNotEmpty() }?.also { ignoreFailures = it.toBoolean() }
+        gradleProps["report.junit.xml"]?.takeIf { it.isNotEmpty() }?.also { reportJunitXmlEnabled = it.toBoolean() }
+        gradleProps["report.junit.html"]?.takeIf { it.isNotEmpty() }?.also { reportJunitHtmlEnabled = it.toBoolean() }
+    }
+
+    reports {
+        junitXml.required.set(reportJunitXmlEnabled)
+        html.required.set(reportJunitHtmlEnabled)
+    }
+
+    useJUnitPlatform {}
+
+    filter {
+        //setIncludePatterns("")
+        //includeTags("")
+        //excludeTags("")
     }
 
     // see org.gradle.api.internal.tasks.testing.logging.DefaultTestLoggingContainer
     //testLogging {}
 
-    // https://docs.gradle.org/current/userguide/performance.html#parallel_test_execution
-    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
-    failFast = false // CI Server => true
-    ignoreFailures = false
-
-    // default value
-    minHeapSize = "256m"
-    maxHeapSize = "512m"
-
     doFirst {
-        val env = "local-test"
         val appPath = "${project.rootDir.absolutePath}/app"
         val configPath = "$appPath/dist/config/$env"
         val swaggerUIPath = "$appPath/dist/swagger-ui"
@@ -67,26 +86,24 @@ tasks.withType<Test> {
             "project.config.dir" to configPath
         )
 
-        val properties = loadProperties("$env.properties")
         if (properties != null) {
-            properties["jvm.minHeapSize"]?.takeIf { it.isNotEmpty() }?.also { minHeapSize = it }
-            properties["jvm.maxHeapSize"]?.takeIf { it.isNotEmpty() }?.also { maxHeapSize = it }
-
             val sysProps = properties.filter { it.key.startsWith("sysProperty.") }
                 .mapKeys { it.key.substring("sysProperty.".length) }.toMutableMap()
-            systemProperties(sysProps)
-            println("========== test system properties ==========")
+            println("========== Testing System Properties ==========")
             println(sysProps)
+
+            systemProperties(sysProps)
 
             val envProps = properties.filter { it.key.startsWith("env.") }
                 .mapKeys { it.key.substring("env.".length) }.toMutableMap()
+            println("========== Testing Environment Variables ==========")
+            println(envProps)
+
             if (envProps["SWAGGER_UI_PATH"].isNullOrBlank())
                 envProps["SWAGGER_UI_PATH"] = swaggerUIPath
             if (envProps["GOOGLE_APPLICATION_CREDENTIALS"].isNullOrBlank())
                 envProps["GOOGLE_APPLICATION_CREDENTIALS"] = firebaseKeyFilePath
             environment(envProps)
-            println("========== test environment variables ==========")
-            println(envProps)
         }
     }
 }
