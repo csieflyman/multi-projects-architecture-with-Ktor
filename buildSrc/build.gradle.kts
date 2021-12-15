8526584d7a36dcf1ffe1d619c31357fc514d9b05
csieflyman <csieflyman@gmail.com>
2021-12-15 13:58:51 +0800

    add gradle dependencies constraint to upgrade log4j-core from 2.14.1 to 2.16.0 required by shadow jar gradle plugin due to Log4jShell security vulnerability. Although it would not affect server at runtime

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,4 +11,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
     implementation("org.jetbrains.kotlin:kotlin-serialization:1.6.0")
     implementation("gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0")
+
+    constraints {
+        implementation("org.apache.logging.log4j:log4j-core:2.16.0") {
+            because("Log4jShell security vulnerability")
+        }
+    }
 }
