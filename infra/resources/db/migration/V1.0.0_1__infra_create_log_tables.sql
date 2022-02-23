dfc0e18d41e17c925914d639e3fd9ab7f82d2c0d
csieflyman <csieflyman@gmail.com>
2022-02-23 17:26:03 +0800

    add missing file

--- infra/resources/db/migration/V1.0.0_1__infra_create_log_tables.sql
+++ infra/resources/db/migration/V1.0.0_1__infra_create_log_tables.sql
@@ -1,75 +1,87 @@
 CREATE TABLE infra_request_log
 (
-    id uuid primary key,
-    req_id varchar(36) NOT NULL,
-    req_at timestamp NOT NULL,
-    api varchar(255) NOT NULL,
-    headers text,
-    querystring text,
-    req_body text,
-    project varchar(20) NOT NULL,
-    function varchar(30) NOT NULL,
-    tag varchar(36),
-    source varchar(30) NOT NULL,
-    tenant_id varchar(20),
-    principal varchar(64) NOT NULL,
-    run_as bool NOT NULL,
-    client_id varchar(36),
+    id             uuid primary key,
+    project        varchar(20) NOT NULL,
+    function       varchar(30) NOT NULL,
+    source         varchar(30) NOT NULL,
+    tenant_id      varchar(20),
+    principal_id   varchar(64),
+    tags           text,
+    user_type      varchar(20),
+    user_id        uuid,
+    run_as         bool,
+    req_id         varchar(36),
+    req_at         timestamp,
+    api            varchar(255),
+    headers        text,
+    querystring    text,
+    req_body       text,
+    ip             varchar(39),
+    client_id      varchar(36),
     client_version varchar(10),
-    ip varchar(39),
-    rsp_at timestamp NOT NULL,
-    rsp_time integer NOT NULL,
-    rsp_status int2 NOT NULL,
-    rsp_body text
+    rsp_at         timestamp,
+    rsp_status     integer,
+    rsp_body       text,
+    rsp_time       integer
 );
 
 create index infra_request_log_req_id_idx on infra_request_log (req_id);
 create index infra_request_log_req_at_idx on infra_request_log (req_at);
+create index infra_request_log_project_idx on infra_request_log (project);
+create index infra_request_log_user_id_idx on infra_request_log (user_id);
 create index infra_request_log_api_idx on infra_request_log (api);
-create index infra_request_log_tag_idx on infra_request_log (tag);
-create index infra_request_log_tenant_id_idx on infra_request_log (tenant_id);
-create index infra_request_log_principal_idx on infra_request_log (principal);
 create index infra_request_log_rsp_time_idx on infra_request_log (rsp_time);
 
 CREATE TABLE infra_error_log
 (
-    id uuid primary key,
-    occur_at timestamp NOT NULL,
-    error_code char(4) NOT NULL,
-    error_code_type varchar(20) NOT NULL,
-    error_msg text,
-    data text,
-    req_id varchar(36),
-    req_at timestamp,
-    api varchar(255),
-    headers text,
-    querystring text,
-    req_body text,
-    project varchar(20),
-    function varchar(30),
-    tag varchar(36),
-    source varchar(30) NOT NULL,
-    tenant_id varchar(20),
-    principal varchar(64),
-    run_as bool NOT NULL,
-    client_id varchar(36),
-    client_version varchar(10),
-    ip varchar(39),
-    rsp_at timestamp,
-    rsp_time integer,
-    service_name varchar(30),
-    service_api varchar(255),
-    service_rsp_code varchar(20),
+    id               uuid primary key,
+    occur_at         timestamp   NOT NULL,
+    error_code       char(4)     NOT NULL,
+    error_code_type  varchar(20) NOT NULL,
+    error_msg        text,
+    stacktrace       text,
+    extras           text,
+    project          varchar(20) NOT NULL,
+    function         varchar(30) NOT NULL,
+    source           varchar(30) NOT NULL,
+    tenant_id        varchar(20),
+    principal_id     varchar(64),
+    tags             text,
+    user_type        varchar(20),
+    user_id          uuid,
+    run_as           bool,
+    req_id           varchar(36),
+    req_at           timestamp,
+    api              varchar(255),
+    headers          text,
+    querystring      text,
+    req_body         text,
+    ip               varchar(39),
+    client_id        varchar(36),
+    client_version   varchar(10),
+    rsp_at           timestamp,
+    rsp_status       integer,
+    rsp_body         text,
+    rsp_time         integer,
+    service_name     varchar(30),
+    service_api      varchar(255),
+    service_req_id   varchar(255),
+    service_req_at   timestamp,
     service_req_body text,
-    service_rsp_body varchar(30)
+    service_rsp_code varchar(20),
+    service_rsp_at   timestamp,
+    service_rsp_body text,
+    service_rsp_time integer
 );
 
-create index infra_error_log_req_id_idx on infra_error_log (req_id);
 create index infra_error_log_occur_at_idx on infra_error_log (occur_at);
-create index infra_error_log_api_idx on infra_error_log (api);
-create index infra_error_log_tag_idx on infra_error_log (tag);
 create index infra_error_log_error_code_idx on infra_error_log (error_code);
 create index infra_error_log_error_code_type_idx on infra_error_log (error_code_type);
+create index infra_error_log_project_idx on infra_error_log (project);
+create index infra_error_log_user_id_idx on infra_error_log (user_id);
+create index infra_error_log_req_id_idx on infra_error_log (req_id);
+create index infra_error_log_api_idx on infra_error_log (api);
+create index infra_error_log_service_name_req_id_idx on infra_error_log (service_name, service_req_id);
 
 CREATE TABLE infra_login_log
 (
