cf5607987035fad1021097f3e2d233bf736fca62
csieflyman <csieflyman@gmail.com>
2023-02-26 16:44:46 +0800

    upgrade kotlinx.serialization 1.5.0

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     // =============== kotlin, kotlinx ===============
     implementation(kotlin("stdlib", kotlinVersion))
     api("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
     // ===============  ktor ===============
